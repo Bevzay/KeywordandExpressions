b30c30b777e7bee6fc5c722689079de369aad35e
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-02-03 14:56:30 +1300

    Expressions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Test
+
+        //---
+
+        // In the following code below, write down what
+        // parts of the code are expressions
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+
+        // score = 100 is an expression
+        // score > 99 is an expression
+        // You got the high score! is an expression
+        // score 0 is an expression
+
+        //---
+
+
+
     }
 }
